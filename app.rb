90f05553b9fed53f310a67c1da7f3b680c5bd9bc
Sophie Neuenmuller <sarisiaice@gmail.com>
2018-09-22 15:46:58 +0900

    impl alias .c to command .chars Since CommandAlias is implemented in discordrb but not released yet, we have to use this way.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ config = YAML.load_file('config.yml')
 bot = Discordrb::Commands::CommandBot.new(token:config['DISCORD_TOKEN'], prefix: ".")
 
 bot.ready{puts 'ready'}
-bot.command :chars, in: '#bot' do |event, unique|
+bot.command [:chars, :c], in: '#bot' do |event, unique|
   characters = HTTP.get("https://tera-lab.appspot.com/user/#{unique}/characters").parse['characters'] rescue []
   if characters.empty?
     event.channel.send_embed do |embed|
